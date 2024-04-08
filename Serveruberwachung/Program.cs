1c4650a860f86b85ce0a81948609082e5bbc3bd8
simonsch484 <simonstier8@gmail.com>
2024-04-08 11:44:54 +0200

    merge fix

--- Serveruberwachung/Program.cs
+++ Serveruberwachung/Program.cs
@@ -10,13 +10,9 @@ class ExampleAuthenticate
     {
         IPConnection ipcon = new IPConnection();
         // //Sensoren
-<<<<<<< HEAD
-        TemperaturSensor sensor = new(ipcon, "Wcg");
-=======
         TemperaturSensor tempSensor = new TemperaturSensor(ipcon, "Wcg");
         LightSensor lightSensor = new LightSensor(ipcon, "Pdw");
         MotionSensor motionSensor = new MotionSensor(ipcon, "ML4");
->>>>>>> b03d0db55e508cdf73f6c469fbb7b250dab4c42c
         // BrickletPiezoSpeakerV2 ps = new("R7M", ipcon);
         // BrickletAmbientLightV3 al = new("",ipcon);
         FeuchtigkeitSensor feucht = new(ipcon, "ViW");
@@ -36,20 +32,14 @@ class ExampleAuthenticate
 
         do {
 
-            //ps.SetBeep(1000, 1, 1000);
-<<<<<<< HEAD
             Thread.Sleep(1000);
-            double temperature = sensor.GetTemperature();
-            Console.WriteLine("Temperature: " + temperature + " °C");
+            tempSensor.GetTemperature();
+            lightSensor.GetLightIntensity();
+            motionSensor.IsMotionDetected();
             feucht.GetHumidity();
-=======
-        Thread.Sleep(1000);
-        tempSensor.GetTemperature();
-        lightSensor.GetLightIntensity();
-        motionSensor.IsMotionDetected();
->>>>>>> b03d0db55e508cdf73f6c469fbb7b250dab4c42c
 
         } while (!Console.KeyAvailable);
         ipcon.Disconnect();
+        Console.WriteLine("disconnected");
     }
 }
